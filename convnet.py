50b8abee1a64a0fcdaa852bb9ee21726c3ac93f4
henry-grafe <lolix3345@gmail.com>
2023-09-03 20:19:17 +0200

    adding depthwise separable convolution CNN
    depthwise separable convolution CNN with modulable width added,

--- convnet.py
+++ convnet.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn as nn
 
 class CNN_modulable_width(nn.Module):
@@ -53,3 +52,80 @@ class CNN_modulable_width(nn.Module):
         #print(out1.size(),out2.size(),out3.size(),out4.size(),out5.size(),out6.size(),out_conv.size())
         out_classifier = self.final_classifier(out_conv)
         return out_classifier
+
+class Depthwise_separable_CNN_modulable_width(nn.Module):
+    def __init__(self, width=16):
+        super(Depthwise_separable_CNN_modulable_width, self).__init__()
+        self.width = width
+        self.conv1 = nn.Conv2d(in_channels=3,
+                          out_channels=self.width,
+                          kernel_size=7,
+                          stride=1,
+                          padding=3)
+
+        self.depthwise_separable_conv2 = Depthwise_Separable_Block(in_features=self.width,
+                                                               out_features=self.width * 2,
+                                                               kernel_size=3,
+                                                               stride=2,
+                                                               padding=1)
+
+        self.depthwise_separable_conv3 = Depthwise_Separable_Block(in_features=self.width * 2,
+                                                               out_features=self.width * 2,
+                                                               kernel_size=3,
+                                                               stride=1,
+                                                               padding=1)
+
+        self.depthwise_separable_conv4 = Depthwise_Separable_Block(in_features=self.width * 2,
+                                                                   out_features=self.width * 4,
+                                                                   kernel_size=3,
+                                                                   stride=2,
+                                                                   padding=1)
+
+        self.depthwise_separable_conv5 = Depthwise_Separable_Block(in_features=self.width * 4,
+                                                                   out_features=self.width * 4,
+                                                                   kernel_size=3,
+                                                                   stride=1,
+                                                                   padding=1)
+
+        self.depthwise_separable_conv6 = Depthwise_Separable_Block(in_features=self.width * 4,
+                                                                   out_features=self.width * 8,
+                                                                   kernel_size=3,
+                                                                   stride=2,
+                                                                   padding=1)
+
+        self.avgpool = nn.AvgPool2d(kernel_size=(4,4))
+        self.final_classifier = nn.Sequential(
+            nn.Linear(self.width*8,self.width*32),
+            nn.ReLU(),
+            nn.Linear(self.width*32,10)
+        )
+
+    def forward(self, input):
+        out1 = nn.functional.relu(self.conv1(input))
+        out2 = nn.functional.relu(self.depthwise_separable_conv2(out1))
+        out3 = nn.functional.relu(self.depthwise_separable_conv3(out2))
+        out4 = nn.functional.relu(self.depthwise_separable_conv4(out3))
+        out5 = nn.functional.relu(self.depthwise_separable_conv5(out4))
+        out6 = nn.functional.relu(self.depthwise_separable_conv6(out5))
+        out_conv = self.avgpool(out6).squeeze()
+        #print(out1.size(),out2.size(),out3.size(),out4.size(),out5.size(),out6.size(),out_conv.size())
+        out_classifier = self.final_classifier(out_conv)
+        return out_classifier
+
+class Depthwise_Separable_Block(nn.Module):
+    def __init__(self, in_features, out_features, kernel_size, stride, padding):
+        super(Depthwise_Separable_Block, self).__init__()
+        self.separable_conv = nn.Conv2d(in_channels=in_features,
+                                        out_channels=in_features,
+                                        kernel_size=kernel_size,
+                                        stride=stride,
+                                        padding=padding,
+                                        groups=in_features)
+
+        self.pointwise_conv = nn.Conv2d(in_channels=in_features,
+                                        out_channels=out_features,
+                                        kernel_size=1,
+                                        stride=1,
+                                        padding=0)
+    def forward(self, input):
+        return self.pointwise_conv(self.separable_conv(input))
